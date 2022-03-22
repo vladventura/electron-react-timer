c543ca67464f5bc9706953f7771cc16ff5e1ede5
Vladimir Ventura <vladventura@outlook.es>
2022-03-22 16:48:54 -0400

    Made ui for clock display

--- src/components/Clock/ClockDisplay.jsx
+++ src/components/Clock/ClockDisplay.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 
 export const ClockDisplay = ({ h, m, s }) => (
     <div className="clock-display">
-        <div className="time-display">
+        <div className="time-display h">
             <p>{Math.round(h)}</p>
+            <div className="display-time-item">H</div>
         </div>
-        <div className="time-display">
+        <div className="time-display m">
             <p>{Math.round(m)}</p>
+            <div className="display-time-item">M</div>
         </div>
-        <div className="time-display">
+        <div className="time-display s">
             <p>{Math.round(s)}</p>
+            <div className="display-time-item">S</div>
         </div>
     </div>
 );
