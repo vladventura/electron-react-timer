2b15f9add88ce12f504fd6e5380e72d9d6253646
Vladimir Ventura <vladventura@outlook.es>
2022-03-22 17:39:41 -0400

    Adding buttons for play and pause, svgs, and css for them

--- webpack.common.js
+++ webpack.common.js
@@ -26,14 +26,15 @@ module.exports = {
       {
         test: [/\.s[ac]ss$/i, /\.css$/i],
         use: [
-          // Creates `style` nodes from JS strings
           'style-loader',
-          // Translates CSS into CommonJS
           'css-loader',
-          // Compiles Sass to CSS
           'sass-loader',
         ],
-      }
+      },
+      {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      },
     ]
   },
   resolve: {
