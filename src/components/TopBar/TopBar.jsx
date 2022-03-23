98e4e84304e539cafe286bf814a665ae38e3c568
Vladimir Ventura <vladventura@outlook.es>
2022-03-23 12:34:48 -0400

    Adding back icon, looks kinda wonky still

--- src/components/TopBar/TopBar.jsx
+++ src/components/TopBar/TopBar.jsx
@@ -1,16 +1,34 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import SettingsIcon from '../../icons/settings.svg'
+import React, { useEffect, useState } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+import SettingsIcon from '../../icons/settings.svg';
+import BackArrow from '../../icons/back-arrow.svg';
 import './TopBar.css';
 
 export const TopBar = () => {
     const nav = useNavigate();
-    const settingsOnClick = () => {
-        nav('/settings');
+    const loc = useLocation();
+    const [Icon, setIcon] = useState(<SettingsIcon />);
+    let onClick = settingsOnClick;
+
+    useEffect(() => { }, []);
+
+    function settingsOnClick() {
+        let pathname = loc.pathname;
+        switch (pathname) {
+            case '/':
+                setIcon(<BackArrow />)
+                nav('/settings');
+                break;
+            case '/settings':
+                setIcon(<SettingsIcon />);
+                nav('/');
+                break;
+        }
     };
+
     return <div className='topbar-container'>
-        <div className="settings-container" onClick={settingsOnClick}>
-            <SettingsIcon />
+        <div className="settings-container" onClick={onClick}>
+            {Icon}
         </div>
     </div>
 };
